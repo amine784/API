dac8372802c3d0b5e21422edb2b9e22599f4632e
amine784 <eminebch@gmail.com>
2022-04-16 14:17:48 +0200

    first push api

---
+++ src/test/java/com/pi/gest/GestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.gest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
